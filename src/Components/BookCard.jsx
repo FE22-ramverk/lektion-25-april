cdfccb67a8846522855d66ee4ea461c6493266d4
Maja Gedda <maja.gedda@zocom.se>
2023-04-25 13:11:56 +0200

    added a component inside a component

--- src/Components/BookCard.jsx
+++ src/Components/BookCard.jsx
@@ -1,25 +1,41 @@
 import { Link } from "react-router-dom";
 
+function BookCardContainerContent({ book }) {
+    return (
+        <>
+            <section className="bookCardContainer__image" style={{
+                width: 300,
+                height: 300,
+                margin: "auto",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center center",
+                backgroundSize: "contain",
+                backgroundImage: `url(${book.imgUrl})`,
+            }}></section>
+            <section className="bookCardContainer__info">
+                <h2>{book.title}</h2>
+                <p>{book.author}</p>
+            </section>
+        </>
+    )
+}
+
 function BookCard({ book, isInMyLibrary }) {
     return (
-        <Link to={'/bookinfo/'+ book.id+ '/' + book.title}>
-            <article className="bookCardContainer">
-                <section className="bookCardContainer__image" style={{
-                    width: 300,
-                    height: 300,
-                    margin: "auto",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center center",
-                    backgroundSize: "contain",
-                    backgroundImage: `url(${book.imgUrl})`,
-                }}></section>
-                <section className="bookCardContainer__info">
-                    <h2>{book.title}</h2>
-                    <p>{book.author}</p>
-                </section>
-                {isInMyLibrary ? <button>Delete</button> : null}
-            </article>
-        </Link>
+        <>
+            {isInMyLibrary ?
+                <article className="bookCardContainer">
+                    <BookCardContainerContent book={book} />
+                    <button>Delete</button>
+                </article>
+                :
+                <Link to={'/bookinfo/' + book.id + '/' + book.title}>
+                    <article className="bookCardContainer">
+                        <BookCardContainerContent book={book} />
+                    </article>
+                </Link>
+            }
+        </>
     );
 }
 
